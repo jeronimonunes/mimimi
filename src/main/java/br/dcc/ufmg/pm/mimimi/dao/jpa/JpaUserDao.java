5f7cc21fb8c312c90c81d5d0f2eea7447b191dfc
Jerônimo Nunes Rocha <jeronimonunes@live.com>
2016-06-09 20:32:11 -0300

    Login

--- src/main/java/br/dcc/ufmg/pm/mimimi/dao/jpa/JpaUserDao.java
+++ src/main/java/br/dcc/ufmg/pm/mimimi/dao/jpa/JpaUserDao.java
@@ -1,5 +1,11 @@
 package br.dcc.ufmg.pm.mimimi.dao.jpa;
 
+import java.math.BigInteger;
+import java.nio.charset.StandardCharsets;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.HashMap;
+
 import br.dcc.ufmg.pm.mimimi.dao.UserDao;
 import br.dcc.ufmg.pm.mimimi.model.User;
 
@@ -9,11 +15,45 @@ public class JpaUserDao extends AbstractJpaDao<String, User> implements UserDao 
 	public Class<User> getEntityClass() {
 		return User.class;
 	}
+	
+	@Override
+	public User save(User entity) {
+		entity.setPassword(getSHA1Code(entity.getPassword()));
+		return super.save(entity);
+	}
+	
+	@Override
+	public User update(User entity) {
+		entity.setPassword(getSHA1Code(entity.getPassword()));
+		return super.update(entity);
+	}
 
 	@Override
-	public void login(String username, String password) {
-		// TODO Auto-generated method stub
-		
+	public User login(String username, String password) {
+		HashMap<String, Object> params = new HashMap<>(2);
+		params.put("login", username);
+		params.put("password", getSHA1Code(password));
+		return super.findSingleResult(User.LOGIN, params);
+	}
+
+	/**
+	 * Method used to apply the SHA-1 hash to a string.
+	 * @see {@link MessageDigest}
+	 * @param s String to be hashed.
+	 * @return The hashed string.
+	 * @throws NoSuchAlgorithmException
+	 */    
+	public static String getSHA1Code(String s) {
+		MessageDigest md = null; 
+		try {
+			md = MessageDigest.getInstance("SHA-1");
+			md.reset();
+			md.update(s.getBytes(StandardCharsets.UTF_8));
+		} catch (NoSuchAlgorithmException e) {
+			throw new RuntimeException(e);
+		}
+		BigInteger hash = new BigInteger(1, md.digest());  
+		return hash.toString(16);  
 	}
 
 }
