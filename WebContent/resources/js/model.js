c6d828cb5098de76bef4a4d9e65103d65025466e
Jerônimo Nunes Rocha <jeronimonunes@live.com>
2016-06-13 21:29:04 -0300

    Mimimi styling

--- WebContent/resources/js/model.js
+++ WebContent/resources/js/model.js
@@ -1,15 +1,38 @@
 function updatePicture(){
-	document.getElementById("picture").src += '&a='+new Date().getTime();
+	var pics = $(".profile-picture");
+	for(var i = 0; i<pics.length;i++){
+		var p = pics[i];
+		if(p.src.indexOf("default.png")==-1){
+			p.src += '&a='+new Date().getTime();
+		} else {
+			p.src = p.src.replace("default.png",$("#username").text()+".jpg");
+		}
+	}
 }
 
 function updateCover(){
-	document.getElementById("cover").src += '&a='+new Date().getTime();
+	var p = document.getElementById("cover");
+	if(p.src.indexOf("default.png")==-1){
+		p.src += '&a='+new Date().getTime();
+	} else {
+		p.src = p.src.replace("default.png",$("#username").text()+".jpg");
+	}
 }
 
 $(document).ready(function(){
-	$("#picture-upload_input").appendTo($("#picture-upload"));
+	$("#picture-upload_input").appendTo($("#picture-upload"))
+	.mouseenter(function(){
+		$("#picture-upload-signal").css("opacity",0.5);
+	}).mouseleave(function(){
+		$("#picture-upload-signal").css("opacity",0)
+	});
 });
 
 $(document).ready(function(){
-	$("#cover-upload_input").appendTo($("#cover-upload"));
+	$("#cover-upload_input").appendTo($("#cover-upload"))
+	.mouseenter(function(){
+		$("#cover-upload-signal").css("opacity",0.5);
+	}).mouseleave(function(){
+		$("#cover-upload-signal").css("opacity",0)
+	});
 });
